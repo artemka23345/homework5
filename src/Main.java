48a76ad3615cdd8a2ecac2c00af628ed385eef3b
Inv <artem@tarabakin.ru>
2023-09-26 01:00:19 +0300

    task done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,88 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task1() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
     }
-}
+    public static void task2() {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+
+        }
+    }
+    public static void task3() {
+        for (int i = 0; i <= 17; i+=2) {
+            System.out.println(i);
+
+        }
+    }
+    public static void task4() {
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+
+        }
+    }
+    public static void task5() {
+        String age = "%d год является високосным %n";
+        for (int i = 1904; i <= 2096; i+=4) {
+            System.out.printf(age,i);
+
+        }
+    }
+    public static void task6() {
+        for (int i = 7; i <= 98; i+=7) {
+            System.out.println(i);
+        }
+    }
+    public static void task7() {
+        for (int i = 1; i <= 512; i*=2) {
+            System.out.println(i);
+        }
+    }
+    public static void task8() {
+        String msg = "Месяц %d, сумма накоплений равна %d рублей %n";
+        int salary = 29000;
+        int money = 0;
+        for (int i = 1; i <= 12; i++) {
+            money = money + salary;
+            System.out.printf(msg,i,money);
+        }
+    }
+    public static void task9() {
+        String msg = "Месяц %d, сумма накоплений равна %.2f рублей %n";
+        int salary = 29000;
+        double money = 0;
+        for (int i = 1; i <= 12; i++) {
+            money = money + money/100;
+            money = money + salary;
+            System.out.printf(msg,i,money);
+        }
+
+    }
+    public static void task10() {
+        int multiplication = 2;
+        int result = 0;
+        for (int i = 1; i <= 10; i++) {
+            result = multiplication * i;
+            System.out.println(multiplication + "*" + i + "=" + result);
+        }
+    }
+    }
+
+
+
+
